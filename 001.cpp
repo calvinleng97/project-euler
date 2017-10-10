3fbfd0f832edb78602a45456154300eb9f0c8152
calvinleng97 <32688766+calvinleng97@users.noreply.github.com>
2017-10-10 15:15:22 -0700

    Added Problem 1.

---
+++ 001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  int result = 0;
+
+  for (int i = 0; i < 1000; i++)
+    if (i % 3 == 0 || i % 5 == 0)
+      result += i;
+
+  cout << result << endl;
+  return 0;
+}
